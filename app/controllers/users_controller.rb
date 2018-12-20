054de5339fc140306dc64e3aed92de044397fe6d
Catherine Salazar <catcreates13@gmail.com>
2018-12-20 02:52:20 +0000

    modify login method and authenticate user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,27 @@ class UsersController < ApplicationController
     erb:login
   end
 
-#receive the login form, find and log the user
+#receive the login form, find, authenticate, log the user and redirect to the user page
   post '/login' do
-    binding.pry
+    #binding.pry
+    @user = User.find_by(username: params[:username])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+      #error message
+      #redirect to log in page
+    end
   end
 
 #render signup page
   get 'signup' do
     erb:signup
   end
+
+#render show page
+  get '/users/:id' do
+    "test"
+  end
+
 end
