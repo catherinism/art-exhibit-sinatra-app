4f27072fa7125a4b3700f0031efc108e3feba4cd
Catherine Salazar <catcreates13@gmail.com>
2018-12-21 01:26:04 +0000

    add sign up page and  signup method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect "/users/#{@user.id}"
+      puts session
+      redirect "users/#{@user.id}"
     else
       #error message
       #redirect to log in page
@@ -19,9 +20,22 @@ class UsersController < ApplicationController
   end
 
 #render signup page
-  get 'signup' do
+  get '/signup' do
+    #raise params.inspect
+
     erb:signup
   end
+
+#receive signup page, create user, and persist in the database
+  post '/users' do
+    #binding.pry
+    if !params.empty?
+    @user = User.create(params)
+    redirect "/users/#{@user.id}"
+  else
+    redirect "/signup"
+  end
+end
 
 #render show page
   get '/users/:id' do
