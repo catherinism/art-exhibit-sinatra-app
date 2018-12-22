a353406fcee028e567f588f87c709b011f7484ec
Catherine Salazar <catcreates13@gmail.com>
2018-12-22 01:42:30 +0000

    add new exhibit page and add new form

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,7 +1,20 @@
 class ExhibitsController < ApplicationController
 
+#render new exhibit page
   get '/exhibits/new' do
-    erb "/exhibits/new"
+    erb :"/exhibits/new"
   end
+
+# receives new exhibit form
+  post '/exhibits' do
+    if !params[:theme].empty?
+      @exhibit = Exhibit.create(params)
+      redirect "/exhibits/#{@exhibit.id}"
+    else
+      redirect "/exhibits/new"
+    end
+
+  end
+
 
 end
