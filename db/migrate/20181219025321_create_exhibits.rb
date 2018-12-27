9d107bdf9359d8f3cae92c1ecc47c5f6a995f632
Catherine Salazar <catcreates13@gmail.com>
2018-12-27 02:20:09 +0000

    add more columns to exhibits table

--- db/migrate/20181219025321_create_exhibits.rb
+++ db/migrate/20181219025321_create_exhibits.rb
@@ -1,7 +1,12 @@
 class CreateExhibits < ActiveRecord::Migration
   def change
     create_table :exhibits do |t|
-      t.string :theme
+      t.string :title
+      t.string :deadline
+      t.string :medium
+      t.string :exhibit_date
+      t.string :location
+      t.string :entry_fee
       t.integer :user_id
 
       t.timestamps null: false
