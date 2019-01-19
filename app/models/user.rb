f6ab4b1119b559c0b29e205e437a7a7e636cf16a
Catherine Salazar <catcreates13@gmail.com>
2019-01-19 03:56:44 +0000

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :exhibits
   has_secure_password
 
+  validates :name, presence: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
+
 end
