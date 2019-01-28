7aca14273939769eebf166eba2d829ae76bd627d
Catherine Salazar <catcreates13@gmail.com>
2019-01-28 02:37:47 +0000

    edit post exhibits flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
   get "/" do
     if logged_in?
-      redirect "/users/#{@current_user.id}"
+      redirect "/users/#{current_user.id}"
     else
       erb :index
     end
